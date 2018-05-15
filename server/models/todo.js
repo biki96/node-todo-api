c50ecac3e7cb799e2cdf9d47bc5f5f7b313f0ac7
Milos Bijanic <bijanicmilos996@gmail.com>
2018-05-15 18:08:33 +0200

    Add POST todo route and refactors mongose

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true //da ne prodje '   ', brise spaces na pocetku i na kraju
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {Todo}; 
