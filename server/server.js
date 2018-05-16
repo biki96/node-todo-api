c556099a5dae45dc673f245ab659ed8340fb179e
Milos Bijanic <bijanicmilos996@gmail.com>
2018-05-16 23:57:17 +0200

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/confgi');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,9 @@ var{User} = require('./models/user');
 var{Todo} = require('./models/todo');
 
 var app = express();
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+var port = process.env.PORT;
+
 
 app.use(bodyParser.json());
 
